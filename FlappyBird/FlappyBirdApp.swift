16a762fdc7e594c1e3227ae30147f66a4fe72fb5
Maxim Makarov <makarovmaxqa@gmail.com>
2023-11-08 22:00:21 +0100

    Initial Commit

---
+++ FlappyBird/FlappyBirdApp.swift
@@ -0,0 +1,17 @@
+//
+//  FlappyBirdApp.swift
+//  FlappyBird
+//
+//  Created by Maxim Makarov on 08.11.2023.
+//
+
+import SwiftUI
+
+@main
+struct FlappyBirdApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
